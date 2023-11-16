64a6ea7ece6a65654d007f73630096c0977fea36
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-16 10:28:09 +0300

    добавил юзеру 2 необязательных поля: characterName и characterUrlImage

--- types/userType.ts
+++ types/userType.ts
@@ -2,6 +2,8 @@ export type userType = {
   displayName: string;
   email: string;
   photoUrl: string | null;
+  characterName?: string | null;
+  characterUrlImage?: string | null;
   stats?: string[]; // пока не уверен
   playedGames?: string[]; // пока не уверен
 };
