47b2c0553509bec865ed5d3a450e3d75377d6734
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-16 10:26:56 +0300

    добавлена константа для цветов персонажей

---
+++ constants/colors.ts
@@ -0,0 +1,21 @@
+export type ColorsCharacters = {
+  bone: string;
+  cactus: string;
+  duck: string;
+  cat: string;
+  zip: string;
+  octocat: string;
+  music: string;
+  heart: string;
+};
+
+export const COLORS_CHARACTERS: ColorsCharacters = {
+  bone: '#54A0FF',
+  cactus: '#1DD1A1',
+  duck: '#FECA57',
+  cat: '#56FFF5',
+  zip: '#5F27CD',
+  octocat: '#FF9FF3',
+  music: '#FF9F43',
+  heart: '#EE5253',
+};
