3727d2be7fd9357684e31671fa9b796e487f3712
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-17 21:05:10 +0300

    исправление форматирования даты

--- utils/formatData.ts
+++ utils/formatData.ts
@@ -1,4 +1,8 @@
 export default function formatData(data: number): string {
   const date = new Date(data);
-  return date.getHours() + ':' + date.getMinutes();
+  return (
+    (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) +
+    ':' +
+    (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+  );
 }
