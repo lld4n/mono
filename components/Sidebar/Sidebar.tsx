582484ef99631fb806e8dbc32903fd61f3c2f2c1
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-18 19:55:22 +0300

    вывод игроков в сайдбаре

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,6 +1,27 @@
 import styles from './Sidebar.module.scss';
 import { gamePlayersType, gameType } from '../../types/gameType';
+import { characters } from '../../assets/characters';
 
 export default function Sidebar({ players }: { players: gamePlayersType }) {
-  return <div></div>;
+  return (
+    <div>
+      {Object.keys(players).map((player, index) => (
+        <div key={index} className={styles['player']}>
+          <div className={styles['player__balance']}>
+            {players[player].balance}
+          </div>
+          <div
+            className={styles['player__name']}
+            style={{
+              backgroundColor:
+                characters[players[player].selected_character].color,
+              color: characters[players[player].selected_character].opposite,
+            }}
+          >
+            {players[player].display_name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
