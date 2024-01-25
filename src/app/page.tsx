ff72d6e6d41392fafbf091993df0424ce4aa9e5d
itrostik <RosASergeev@stud.kpfu.ru>
2024-01-25 19:31:24 +0300

    сделаны компоненты Primary и Secondary + прикручены к главной странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div>Page</div>;
+"use client";
+import { useConvexAuth } from "convex/react";
+import Loading from "@/components/Loading/Loading";
+import Secondary from "@/components/Secondary/Secondary";
+import Primary from "@/components/Primary/Primary";
+export default function Home() {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (!isAuthenticated) {
+    return <Primary />;
+  }
+  return <Secondary />;
 }
---
+++ src/components/Secondary/Secondary.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Secondary.module.scss";
+import Link from "next/link";
+import UserInfo from "@/components/UserInfo/UserInfo";
+import { api } from "../../../convex/_generated/api";
+import { useMutation } from "convex/react";
+import { Id } from "../../../convex/_generated/dataModel";
+
+export default function Secondary() {
+  const mutation = useMutation(api.users.store);
+  const [userId, setUserId] = useState<Id<"users">>();
+  useEffect(() => {
+    const getUser = async () => {
+      const userId = await mutation();
+      setUserId(userId);
+    };
+    getUser();
+  }, []);
+  return (
+    <div className={styles.content}>
+      <Link href={"/rules"} className={styles.item}>
+        Правила
+      </Link>
+      <Link href={"/open"} className={styles.item}>
+        Открытые комнаты
+      </Link>
+      <Link href={"/come"} className={styles.item}>
+        Войти в комнату
+      </Link>
+      <Link href={"/room"} className={styles.itemLast}>
+        Создать комнату
+      </Link>
+      {userId && (
+        <div className={styles.user}>
+          <UserInfo users_id={userId} />
+        </div>
+      )}
+    </div>
+  );
+}
