8d5c1ed5b2749f6fc4b43c435c78784b3dfafa84
lldan <safinildan@gmail.com>
2024-01-25 12:48:44 +0300

    добавлен convex provider + clerk provider

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://creative-polecat-29.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/providers/ConvexClientProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
