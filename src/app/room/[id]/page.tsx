1aaec38d6ba01cc9250200e9fc64098c1447cfd5
itrostik <RosASergeev@stud.kpfu.ru>
2024-01-27 11:16:49 +0300

    сделан компонент, но какая-то суета с добавлением игрока в комнату происходит, пока не разобрался

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,3 +1,37 @@
-export default function Game() {
-  return <div>52</div>;
+"use client";
+import RoomFigureSelect from "@/components/RoomFigureSelect/RoomFigureSelect";
+import Copy from "@/components/Copy/Copy";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import styles from "./page.module.scss";
+
+export default function Game({ params }: { params: { id: Id<"games"> } }) {
+  const [playerId, setPlayerId] = useState<Id<"players">>();
+
+  const players = useQuery(api.players.getAllByGames, {
+    games_id: params.id,
+  });
+  const mutation = useMutation(api.players.add);
+  useEffect(() => {
+    const addPlayer = async () => {
+      const playerId = await mutation({ games_id: params.id });
+      setPlayerId(playerId);
+    };
+    addPlayer();
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <Copy value={params.id} />
+      {players && playerId && (
+        <RoomFigureSelect
+          games_id={params.id}
+          players_id={playerId}
+          players={players}
+        />
+      )}
+    </div>
+  );
 }
---
+++ src/components/RoomFigureSelect/RoomFigureSelect.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { PlayersGetType } from "@/utils/PlayersGetType";
+import { Id } from "../../../convex/_generated/dataModel";
+import { figuresList } from "@/constants/figures";
+import styles from "./RoomFigureSelect.module.scss";
+
+import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
+type PropsType = {
+  players: PlayersGetType[];
+  games_id: Id<"games">;
+  players_id: Id<"players">;
+};
+
+export default function RoomFigureSelect({
+  players,
+  games_id,
+  players_id,
+}: PropsType) {
+  const mutation = useMutation(api.players.select);
+  async function select(figureIndex: number, isSelected: boolean) {
+    if (!isSelected) {
+      await mutation({ players_id, games_id, selected: figureIndex });
+    }
+  }
+
+  return (
+    <div>
+      <h3 className={styles.title}>
+        Выберите персонажа, за которого будете играть
+      </h3>
+      <div className={styles.figureList}>
+        {figuresList.map((figure) => {
+          let isSelected = false;
+          players.forEach((player) => {
+            if (player.selected === figure.index) {
+              isSelected = true;
+              return;
+            }
+          });
+          return (
+            <button
+              key={figure.index}
+              className={isSelected ? styles.disabled : styles.active}
+              onClick={() => select(figure.index, isSelected)}
+            >
+              <Image src={figure.svg} alt={"figure"} />
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
