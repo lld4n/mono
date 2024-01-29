09f52cb9408dc3b14ad447a33bdd8549001c5a0f
itrostik <RosASergeev@stud.kpfu.ru>
2024-01-29 11:35:20 +0300

    рендер списка игроков готов

--- src/components/Game/GamePlayersList/GamePlayersList.tsx
+++ src/components/Game/GamePlayersList/GamePlayersList.tsx
@@ -1,10 +1,10 @@
 import { Doc } from "../../../../convex/_generated/dataModel";
 import { PlayersGetType } from "@/types/PlayersGetType";
-import { figuresList } from "@/constants/figures";
 import styles from "./GamePlayersList.module.scss";
 import { GetFigureFromSelected } from "@/utils/GetFigureFromSelected";
-import { Crown } from "lucide-react";
 import Image from "next/image";
+import Money from "@/components/Game/Money/Money";
+import { XSquare } from "lucide-react";
 
 type PropsType = {
   players: PlayersGetType[];
@@ -13,28 +13,60 @@ type PropsType = {
 export default function GamePlayersList({ players, game }: PropsType) {
   return (
     <div className={styles.wrapper}>
-      {players &&
-        players.map((player) => {
-          const figure = GetFigureFromSelected(player);
-          return (
-            <div key={player.user?._id} className={styles.player}>
-              <div
-                className={styles.figure}
-                style={{
-                  backgroundColor: figure.bg,
-                }}
-              ></div>
-              <Image
-                src={player.user?.picture!}
-                alt={"avatar"}
-                width={30}
-                height={30}
-                className={styles.avatar}
-              />
-              <span className={styles.playerName}>{player.user?.name}</span>
-            </div>
-          );
-        })}
+      <div className={styles.players}>
+        {players &&
+          players.map((player) => {
+            const figure = GetFigureFromSelected(player);
+            return (
+              <>
+                {!player.loser ? (
+                  <div
+                    key={player.user?._id}
+                    className={
+                      player.order === game.current
+                        ? styles.player + " " + styles.active
+                        : styles.player
+                    }
+                  >
+                    <div
+                      className={styles.figure}
+                      style={{
+                        backgroundColor: figure.bg,
+                      }}
+                    ></div>
+                    <Image
+                      src={player.user?.picture!}
+                      alt={"avatar"}
+                      width={30}
+                      height={30}
+                      className={styles.avatar}
+                    />
+                    <span className={styles.playerName}>
+                      {player.user?.name}
+                    </span>
+                    <div className={styles.balance}>
+                      <Money value={player.balance} />
+                    </div>
+                  </div>
+                ) : (
+                  <div className={styles.player}>
+                    <Image
+                      src={player.user?.picture!}
+                      alt={"avatar"}
+                      width={30}
+                      height={30}
+                      className={styles.avatar}
+                    />
+                    <span className={styles.playerName}>
+                      {player.user?.name}
+                    </span>
+                    <XSquare size={16} color={"#fff"} />
+                  </div>
+                )}
+              </>
+            );
+          })}
+      </div>
     </div>
   );
 }
