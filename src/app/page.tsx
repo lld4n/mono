8a59433815dd0c978fdb4637bc371b0b7c83848a
lldan <safinildan@gmail.com>
2024-01-29 21:37:44 +0300

    убрана проверка isLoading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 import { useConvexAuth } from "convex/react";
-import Loading from "@/components/Global/Loading/Loading";
 import Secondary from "@/components/Home/Secondary/Secondary";
 import Primary from "@/components/Home/Primary/Primary";
 import Authors from "@/components/Home/Authors/Authors";
 export default function Home() {
-  const { isLoading, isAuthenticated } = useConvexAuth();
-  if (isLoading) {
-    return <Loading />;
-  }
+  const { isAuthenticated } = useConvexAuth();
+
   if (!isAuthenticated) {
     return (
       <>
