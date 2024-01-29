77844f55a56701404d122665ae5db44148c8eb05
lldan <safinildan@gmail.com>
2024-01-29 06:07:04 +0300

    build

--- convex/cards.ts
+++ convex/cards.ts
@@ -2,6 +2,7 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { CardsGetType } from "../src/types/CardsGetType";
 import { CardClassObject, CardGroupObject } from "../src/constants/cardsObject";
+import { Doc } from "./_generated/dataModel";
 
 export const getByGames = query({
   args: {
@@ -92,5 +93,58 @@ export const buy = mutation({
     } else {
       throw new Error("Класс карточки не получилось определить");
     }
+  },
+});
+export const build = mutation({
+  args: {
+    cards_id: v.id("cards"),
+    players_id: v.id("players"),
+    money: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const player = await ctx.db.get(args.players_id);
+    const card = await ctx.db.get(args.cards_id);
+    if (card === null) {
+      throw new Error("Карточка не найден");
+    }
+    if (player === null) {
+      throw new Error("Игрок не найден");
+    }
+    if (card.owner !== player._id) {
+      throw new Error("Карточка не принадлежит игроку");
+    }
+    if (card.mortgage) {
+      throw new Error("Карточка заложена");
+    }
+    if (player.balance < args.money) {
+      throw new Error("Недостаточно денег");
+    }
+    if (CardClassObject[card.index] !== "street") {
+      throw new Error("Карточка не улица");
+    }
+    const group = CardGroupObject[card.index];
+    const convexCards = await ctx.db
+      .query("cards")
+      .filter((q) => q.eq(q.field("games_id"), card.games_id))
+      .collect();
+    const cardsGroup: Doc<"cards">[] = [];
+    for (const item of convexCards) {
+      if (
+        group.includes(item.index) &&
+        !item.mortgage &&
+        item.owner === player._id
+      ) {
+        cardsGroup.push(item);
+      }
+    }
+    if (group.length !== cardsGroup.length) {
+      throw new Error("Не все карточки из группы принадлежат игроку");
+    }
+    await ctx.db.patch(player._id, {
+      balance: player.balance - args.money,
+    });
+    await ctx.db.patch(card._id, {
+      status: card.status + 1,
+    });
   },
 });
