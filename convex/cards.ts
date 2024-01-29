1c3dde7b9c8ffacb3b9d40f8e6d6fb740e0a94dd
lldan <safinildan@gmail.com>
2024-01-29 05:52:04 +0300

    buy + отредачены константы и тип

--- convex/cards.ts
+++ convex/cards.ts
@@ -1,6 +1,7 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { CardsGetType } from "../src/types/CardsGetType";
+import { CardClassObject, CardGroupObject } from "../src/constants/cardsObject";
 
 export const getByGames = query({
   args: {
@@ -11,10 +12,85 @@ export const getByGames = query({
       .query("cards")
       .filter((q) => q.eq(q.field("games_id"), args.games_id))
       .collect();
+    if (convexCards.length === 0) {
+      throw new Error("Карточек нет");
+    }
     const res: CardsGetType[] = new Array(40).fill(null);
     for (const card of convexCards) {
       res[card.index] = card;
     }
     return res;
+  },
+});
+
+export const buy = mutation({
+  args: {
+    cards_id: v.id("cards"),
+    players_id: v.id("players"),
+    money: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const player = await ctx.db.get(args.players_id);
+    const card = await ctx.db.get(args.cards_id);
+    if (card === null) {
+      throw new Error("Карточка не найден");
+    }
+    if (player === null) {
+      throw new Error("Игрок не найден");
+    }
+    if (card.owner) {
+      throw new Error("У карточки есть владелец");
+    }
+    if (player.balance < args.money) {
+      throw new Error("Недостаточно денег");
+    }
+    await ctx.db.patch(player._id, {
+      balance: player.balance - args.money,
+    });
+    const cardClass = CardClassObject[card.index];
+    if (cardClass === "street") {
+      await ctx.db.patch(card._id, {
+        owner: player._id,
+        buy: false,
+        status: card.status + 1,
+      });
+    } else if (cardClass === "train" || cardClass === "nature") {
+      const group = CardGroupObject[card.index];
+      if (group.length === 0) {
+        throw new Error("Группу карточки не получилось определить");
+      }
+      await ctx.db.patch(card._id, {
+        owner: player._id,
+        buy: false,
+      });
+      const cardsGroup = await ctx.db
+        .query("cards")
+        .filter((q) =>
+          q.and(
+            q.eq(q.field("games_id"), card.games_id),
+            q.eq(q.field("owner"), player._id),
+          ),
+        )
+        .collect();
+      if (cardsGroup.length === 0) {
+        throw new Error("Список группы пуст");
+      }
+      let count = 0;
+      for (const item of cardsGroup) {
+        if (group.includes(item.index)) {
+          count++;
+        }
+      }
+      // тут как будто поплыл я немного
+      for (const item of cardsGroup) {
+        if (group.includes(item.index)) {
+          await ctx.db.patch(item._id, {
+            status: count - 1,
+          });
+        }
+      }
+    } else {
+      throw new Error("Класс карточки не получилось определить");
+    }
   },
 });
--- src/types/card/CardClassObjectType.ts
+++ src/types/card/CardClassObjectType.ts
@@ -1,5 +1,5 @@
-import { CardClassOwnerType } from "@/types/card/CardClassOwnerType";
-
+import { CardClassOwnerType } from "../../types/card/CardClassOwnerType";
+// ни в коем случае не менять импорт на @, так как тогда convex сломается
 export type CardClassObjectType = {
   [key: number]: CardClassOwnerType;
 };
