4b4ae1128d8c4bfeb11e04996f3244e0a4c60e38
lldan <safinildan@gmail.com>
2024-01-29 23:03:09 +0300

    Game Footer

--- src/components/Game/GameFooter/GameFooter.tsx
+++ src/components/Game/GameFooter/GameFooter.tsx
@@ -4,26 +4,24 @@ import Money from "@/components/Game/Money/Money";
 import Timer from "@/components/Game/Timer/Timer";
 
 type PropsType = {
-  currentPlayer: Doc<"players"> | undefined | null;
-  game: Doc<"games"> | undefined;
+  currentPlayer: Doc<"players">;
+  game: Doc<"games">;
 };
 
 export default function GameFooter({ currentPlayer, game }: PropsType) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.left}>
-        {game && (
-          <div className={styles.leftItem}>
-            <Timer time={game?.started} />
-          </div>
-        )}
+      <div className={styles.block}>
         <div className={styles.leftItem}>
-          <Money value={currentPlayer?.balance} />
+          <Timer time={game.started} />
+        </div>
+        <div className={styles.leftItem}>
+          <Money value={currentPlayer.balance} />
         </div>
       </div>
-      <div className={styles.right}>
+      <div className={styles.block}>
         <div className={styles.item}>Обмен</div>
-        <div className={styles.item}>Покинуть игру</div>
+        <div className={styles.delete}>Покинуть игру</div>
       </div>
     </div>
   );
