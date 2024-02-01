df9b49991976b3f9af22c917327d24b8268231a3
lldan <safinildan@gmail.com>
2024-02-02 01:58:06 +0300

    отредачены авторы

--- src/components/Home/Authors/Authors.tsx
+++ src/components/Home/Authors/Authors.tsx
@@ -3,7 +3,6 @@ import styles from "./Authors.module.scss";
 export default function Authors() {
   return (
     <div className={styles.wrapper}>
-      created by{" "}
       <a href="https://lldan.ru" className={styles.link}>
         lldan
       </a>{" "}
