ce28cd38128b2314c026be8c79af3a3297f7eee5
lldan <safinildan@gmail.com>
2024-02-02 02:13:34 +0300

    Button + Primary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,22 @@ import { useConvexAuth } from "convex/react";
 import Secondary from "@/components/Home/Secondary/Secondary";
 import Primary from "@/components/Home/Primary/Primary";
 import Authors from "@/components/Home/Authors/Authors";
+import styles from "./page.module.scss";
 export default function Home() {
   const { isAuthenticated } = useConvexAuth();
 
   if (!isAuthenticated) {
     return (
-      <>
+      <div className={styles.wrapper}>
         <Primary />
         <Authors />
-      </>
+      </div>
     );
   }
   return (
-    <>
+    <div className={styles.wrapper}>
       <Secondary />
       <Authors />
-    </>
+    </div>
   );
 }
