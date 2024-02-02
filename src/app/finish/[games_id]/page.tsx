34487d07b16dcaa384393d7c91730c01b9a87ef4
lldan <safinildan@gmail.com>
2024-02-02 04:46:19 +0300

    Finish

--- src/app/finish/[games_id]/page.tsx
+++ src/app/finish/[games_id]/page.tsx
@@ -1,5 +1,29 @@
+"use client";
 import React from "react";
+import { useAction } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import Link from "next/link";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-export default function Finish() {
-  return <div>победитель определен</div>;
+export default function Finish({ params }: { params: { games_id: Id<"games"> } }) {
+  const removeGame = useAction(api.games.remove);
+  const router = useRouter();
+  React.useEffect(() => {
+    toast.promise(removeGame({ games_id: params.games_id }), {
+      loading: "Очищаем бд",
+      success: () => {
+        router.push("/");
+        return "Очистка завершена";
+      },
+      error: (error) => error,
+    });
+  }, []);
+  return (
+    <div>
+      Победитель на базе
+      <Link href="/">На базу</Link>
+    </div>
+  );
 }
