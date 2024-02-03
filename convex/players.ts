3fb190ee7fb4bb949efff6c81826afdf277341b1
lldan <safinildan@gmail.com>
2024-02-03 09:31:45 +0300

    че

--- convex/players.ts
+++ convex/players.ts
@@ -31,7 +31,7 @@ export const add = mutation({
       const player_id = await ctx.db.insert("players", {
         selected: -1,
         position: 0,
-        balance: 1500000,
+        balance: 15000,
         loser: false,
         user: user._id,
         games_id: args.games_id,
